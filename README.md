539fa1a0d415d68ed1ca6a3c361b3f0ac2bf36e8
JinShil <slavo5150@yahoo.com>
2018-06-13 15:31:01 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # memcpyD
 An experiment to explore implementing memcpy in D
+
+## Disclaimer
+* I'm not a very good D programmer
+* I've not very experienced with Intel architectures
+* I'm not very knowledgeable about memory hierarchies
+* I don't have much experience benchmarking code
+* Currently only implements memory-aligned types with sizes in powers of 2
+
+I'm just trying to reason through this as best as I can
+
+## Why?
+Some things go without saying.
+
+## Results so far
+
+Compiled with `dmd -m64 memcpyd.d`.
+![](https://raw.githubusercontent.com/JinShil/memcpyD/master/images/win10.png)
+
+Compiled with `dmd memcpyd.d`.
+![](https://raw.githubusercontent.com/JinShil/memcpyD/master/images/linux.png)
+
+Graphs generated with gnuplot:
+```
+set autoscale
+set logscale xy 2
+plot 'data.txt' using "size":"memcpyC" with lines, '' using "size":"memcpyD" with lines
+```
---
+++ memcpyd.d
@@ -0,0 +1,211 @@
+import std.datetime.stopwatch;
+import core.stdc.string;
+import std.random;
+import std.traits;
+import std.stdio;
+
+void verify(T)(const scope T a, const scope T b)
+{
+    assert(memcmp(&a, &b, T.sizeof) == 0);
+}
+
+void memcpyC(T)(T* src, T* dst)
+{
+    memcpy(dst, src, T.sizeof);
+}
+
+void memcpyD(T)(T* src, T* dst)
+    if (isScalarType!T)
+{
+    // writeln("Copying for " ~ T.stringof);
+    *dst = *src;
+}
+
+void memcpyD(T)(T* src, T* dst)
+    if (is(T == struct))
+{
+    // writeln("Copying for " ~ T.stringof);
+    static if (!is(T == ubyte) && T.sizeof == 1)
+    {
+        memcpyD(cast(ubyte*)src, cast(ubyte*)dst);
+        return;
+    }
+    else static if (!is(T == ushort) && T.sizeof == 2)
+    {
+        memcpyD(cast(ushort*)src, cast(ushort*)dst);
+        return;
+    }
+    else static if (!is(T == uint) && T.sizeof == 4)
+    {
+        memcpyD(cast(uint*)src, cast(uint*)dst);
+        return;
+    }
+    else static if (!is(T == ulong) && T.sizeof == 8)
+    {
+        memcpyD(cast(ulong*)src, cast(ulong*)dst);
+        return;
+    }
+    else static if (T.sizeof >= 16 && T.sizeof < 1024)
+    {
+        import core.simd;
+        import std.conv;
+        void16* s = cast(void16*)src;
+        void16* d = cast(void16*)dst;
+
+        static foreach(i; 0 .. T.sizeof/16)
+        {
+            mixin("*(d+" ~ i.to!string ~ ") = *(s+" ~ i.to!string ~ ");");
+        }
+
+        return;
+    }
+    else
+    {
+        asm pure nothrow @nogc
+        {
+            mov RSI, src;
+            mov RDI, dst;
+            cld;
+            mov RCX, T.sizeof;
+            rep;
+            movsb;
+        }
+    }
+}
+
+Duration benchmark(T, alias f)(T* src, T* dst)
+{
+    enum iterations = 10_000_000;
+    Duration result;
+    auto sw = StopWatch(AutoStart.yes);
+
+    sw.reset();
+    foreach (_; 0 .. iterations)
+    {
+        f(src, dst);
+    }
+    result = sw.peek();
+
+    return result;
+}
+
+void init(T)(ref T v)
+{
+    static if (is (T == float))
+    {
+        v = uniform(0.0f, float.max);
+    }
+    else static if (is(T == double))
+    {
+        v = uniform(0.0, double.max);
+    }
+    else static if (is(T == real))
+    {
+        v = uniform(0.0L, real.max);
+    }
+    else
+    {
+        static if (T.sizeof == 1)
+        {
+            v = cast(T)(uniform!ubyte);
+        }
+        else static if (T.sizeof == 2)
+        {
+            v = cast(T)(uniform!ushort);
+        }
+        else static if (T.sizeof == 4)
+        {
+            v = cast(T)(uniform!uint);
+        }
+        else static if (T.sizeof == 8)
+        {
+            v = cast(T)(uniform!ulong);
+        }
+        else
+        {
+            auto m = (cast(ubyte*) &v)[0 .. T.sizeof];
+            for(int i = 0; i < m.length; i++)
+            {
+                m[i] = uniform!byte;
+            }
+        }
+    }
+}
+
+void test(T)()
+{
+    T d;
+    T s;
+
+    init(d);
+    init(s);
+    memcpyC(&s, &d);
+    verify(s, d);
+
+    init(d);
+    init(s);
+    memcpyD(&s, &d);
+    verify(s, d);
+
+    auto d1 = benchmark!(T, memcpyC)(&s, &d);
+    auto d2 = benchmark!(T, memcpyD)(&s, &d);
+
+    writeln(T.sizeof, " ", d1.total!"usecs", " ", d2.total!"usecs");
+}
+
+struct S1 { ubyte x; }
+struct S2 { ushort x; }
+struct S4 { uint x; }
+struct S8 { ulong x; }
+struct S16 { ubyte[16] x; }
+struct S32 { ubyte[32] x; }
+struct S64 { ubyte[64] x; }
+struct S128 { ubyte[128] x; }
+struct S256 { ubyte[256] x; }
+struct S512 { ubyte[512] x; }
+struct S1024 { ubyte[1024] x; }
+struct S2048 { ubyte[2048] x; }
+struct S4096 { ubyte[4096] x; }
+struct S8192 { ubyte[8192] x; }
+struct S16384 { ubyte[16384] x; }
+struct S32768 { ubyte[32768] x; }
+struct S65536 { ubyte[65536] x; }
+
+void main()
+{
+    // For performing benchmarks
+    writeln("size memcpyC memcpyD");
+    test!S1;
+    test!S2;
+    test!S4;
+    test!S8;
+    test!S16;
+    test!S32;
+    test!S64;
+    test!S128;
+    test!S256;
+    test!S512;
+    test!S1024;
+    test!S2048;
+    test!S4096;
+    test!S8192;
+    test!S16384;
+    test!S32768;
+    test!S65536;
+
+    // For testing integrity
+    writeln("");
+    writeln("size memcpyC memcpyD");
+    test!bool;
+    test!ubyte;
+    test!byte;
+    test!ushort;
+    test!short;
+    test!uint;
+    test!int;
+    test!ulong;
+    test!long;
+    test!float;
+    test!double;
+    test!real;
+}
